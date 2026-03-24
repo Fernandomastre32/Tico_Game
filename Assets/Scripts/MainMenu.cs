e33953928f7b1f56798841daab5e371e2b3e7b26
Fernando <20233L001062@utcv.edu.mx>
2026-03-23 22:09:19 -0600

    fix:size ui

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -28,18 +28,19 @@ public class MainMenu : MonoBehaviour
     // NAVEGACIÓN ENTRE PANELES
     // --------------------------------------------------------
     
-    public void ShowMainMenu() 
+  public void ShowMainMenu() 
     { 
+        // Nos aseguramos de que el menú principal esté prendido y apagamos el popup de opciones
         if(mainMenuPanel != null) mainMenuPanel.SetActive(true); 
         if(optionsMenuPanel != null) optionsMenuPanel.SetActive(false); 
     }
 
     public void ShowOptionsMenu() 
     { 
+        // Solo encendemos el panel de opciones. 
+        // Borramos la línea que apagaba el menú principal para que se quede de fondo.
         if(optionsMenuPanel != null) optionsMenuPanel.SetActive(true); 
-        if(mainMenuPanel != null) mainMenuPanel.SetActive(false); 
     }
-
     // --------------------------------------------------------
     // ACCIONES DEL JUEGO
     // --------------------------------------------------------
